a4b391ede9dc7eb70f486a6d264403613373f137
Vinícius Diller <viniciusdiller@hotmail.com>
2025-09-17 11:49:58 -0300

    refactor: Tags Cursos Default

--- components/ui/tagsanimate.tsx
+++ components/ui/tagsanimate.tsx
@@ -13,7 +13,7 @@ export default function TagsAnimadas() {
           initial={{ opacity: 0, scale: 0.8, y: 20 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{ duration: 0.4, delay: i * 0.1, type: "spring" }}
-          className="px-3 py-1 text-xs font-semibold rounded-full text-gray-800 shadow-md "
+          className="px-3 py-1 text-xs font-semibold rounded-full text-gray-800 shadow-md hover:cursor-default"
           style={{
             backgroundColor: ["#f9fafb"][Math.floor(Math.random() * 1)],
           }}
