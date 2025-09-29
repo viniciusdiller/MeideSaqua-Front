77c960714f616d603b968c2b2af3e965c1bd6892
Micael Robert <micael.robert23@gmail.com>
2025-09-29 11:38:54 -0300

    fix: servert configs

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,9 @@ const nextConfig = {
     domains: ['placeholder.svg'],
     unoptimized: true,
   },
+  experimental: {
+    allowedDevOrigins: ['http://172.16.32.199:3000'],
+  },
 }
 
 export default nextConfig
